c2f1fff79b1c39d7ef60b95b07a9567f1c6969a2
Maximilliar <maximmakar@gmail.com>
2021-09-28 22:42:28 +0600

    add new functions, fix code

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
 import modals from "./modals/modals";
+import sliders from "./modals/sliders";
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
     modals();
+    sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
+    sliders('.main-slider-item', 'vertical', '', '');
 });
