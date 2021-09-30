94c5538dad89a08b6ea8db07b6f1749468a9805a
Maximilliar <maximmakar@gmail.com>
2021-09-30 21:44:00 +0600

    Add new functions

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 import modals from "./modals/modals";
 import sliders from "./modals/sliders";
+import forms from "./modals/forms";
+import mask from "./modals/mask";
+import checkTextInputs from "./modals/checkTextInputs";
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -7,4 +10,8 @@ window.addEventListener('DOMContentLoaded', () => {
     modals();
     sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical', '', '');
+    forms();
+    mask('[name="phone"]');
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
 });
